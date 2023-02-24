1e603920141b87451865b9ae27ed78a6bd24e85a
ploiseau <patrick.loiseau@ensae.fr>
2023-02-24 14:39:30 +0000

    added basic functions

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,8 +1,5 @@
 from graph import Graph, graph_from_file
 
 
-data_path = "input/"
-file_name = "network.01.in"
-
-g = graph_from_file(data_path + file_name)
+g = graph_from_file("input/network.00.in")
 print(g)
